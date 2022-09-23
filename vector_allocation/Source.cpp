5e9c21061a3b273d583e9b5d8596dec674737fb1
AzoiAzoi <98526313+AzoiAzoi@users.noreply.github.com>
2022-09-23 14:35:49 +0200

    Add project files.

---
+++ vector_allocation/Source.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <math.h>
+#include <vector>
+using namespace std;
+
+int main() {
+	int amount{};
+	cout << "Enter the amount of elements you want in the vector: "; cin >> amount;
+	auto* vector_arr{ new vector<float>{} };
+
+
+	for (int i{}; i < amount; i++) {
+		(*vector_arr).push_back(1 / pow((i + 1), 2));
+	}
+
+	float sum{};
+	for (int j{}; j < amount; j++) {
+		sum += vector_arr->at(j);
+	}
+	sum *= 6;
+	cout << "Square root of the sum :" << sqrt(sum);
+
+
+	return 0;
+}
